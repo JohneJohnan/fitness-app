294df2ff31d3f421ebeeb168414308d384d6b7bb
Johnny <kggmmj@gmail.com>
2023-11-30 22:38:27 +0330

    Delete most of the project to an empty project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import Header from "./assets/layout/header.jsx";
-import SideBar from "./assets/layout/sideBar.jsx";
+
 
 export default function App() {
   return (
     <>
-      <SideBar />
-      <Header/>
-      <main>
-        <p>main</p>
-      </main>
     </>
   );
 }
