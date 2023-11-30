0f67023781e86ffe41836ba81eb948d831dfbc94
Johnny <kggmmj@gmail.com>
2023-11-30 22:22:56 +0330

    Add right side of header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import Header from "./assets/layout/header.jsx";
 import SideBar from "./assets/layout/sideBar.jsx";
 
 export default function App() {
-  return (<SideBar/>);
+  return (
+    <>
+      <SideBar />
+      <Header/>
+      <main>
+        <p>main</p>
+      </main>
+    </>
+  );
 }
--- src/assets/layout/header.jsx
+++ src/assets/layout/header.jsx
@@ -1,5 +1,17 @@
-import classes from "./header.module.css"
+import classes from "./header.module.css";
+import avatar from '../icons/profile-circle-svgrepo-com.svg';
+import drop from '../icons/dropdown-arrow-svgrepo-com.svg';
 
-export default function header() {
-
+export default function Header() {
+  return(
+    <header className={classes.header}>
+      <div className={classes.profile}>
+        <img src={avatar} alt="your profile picture" />
+        <img src={drop}/>
+        <p>نام و نام خانوادگی</p>
+        <p>خوش آمدید!</p>
+      </div>
+      <div className={classes.options}></div>
+    </header>
+  );
 }
